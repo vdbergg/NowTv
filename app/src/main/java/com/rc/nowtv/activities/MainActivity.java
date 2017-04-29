c534560dd3d396d7b689d7158e080fb302e694b0
Berg <vandenberg.jutai@gmail.com>
2017-04-29 12:06:09 -0400

    Novo foto

--- app/src/main/java/com/rc/nowtv/activities/MainActivity.java
+++ app/src/main/java/com/rc/nowtv/activities/MainActivity.java
@@ -268,7 +268,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                                 }
                             });
 
-                            Log.d("App", chat.toString());
+                            Log.d(TAG, chat.toString());
                         }
                     });
                 }
